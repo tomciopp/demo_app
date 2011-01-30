bc2c6573bfbbc3587b0d2a5ca7bb347c77ed4562
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2011-01-29 19:20:40 -0500

    fixed the recaptcha

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DemoApp::Application.initialize!
-config.gem 'ruby-recaptcha'
+
 RCC_PUB='6LdK-MASAAAAAM1u3BGUNin-yMM2-R9ZZvd-AWoV'
 RCC_PRIV='6LdK-MASAAAAABQSvjvxZYocriyMOC-lDp3p4IHW'
 
